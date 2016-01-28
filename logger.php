fdce96a5c9083dbac2a9cbf1cc290130f96590c5
lloydzhou <lloydzhou@qq.com>
2016-01-28 20:45:34 +0800

    format code and move cond call outside

--- logger.php
+++ logger.php
@@ -1,16 +1,29 @@
 <?php
+/**
+ * @lloydzhou https://github.com/bephp/logger
+ */
+
 function logger($path, $cond = true) {
-   $logs = array();
-   register_shutdown_function(function() use ($path, &$logs){
-       $fd = fopen($path, 'a+');
-       foreach($logs as $log){
-           $msg = count($log) > 1 ? call_user_func_array('sprintf', $log) : array_shift($log);
-           if($msg) fwrite($fd, $msg."\n");
-       }
-       fclose($fd);
-   });
-   return function () use ($cond, &$logs) {
-       return (is_callable($cond) ? call_user_func($cond) : !!($cond))
-           ? $logs[] = func_get_args() : false;
-   };
+
+    $logs = array();
+
+    register_shutdown_function(function() use ($path, &$logs){
+
+        $fd = fopen($path, 'a+');
+
+        foreach($logs as $log)
+            if ($msg = count($log) > 1 
+                ? call_user_func_array('sprintf', $log)
+                : array_shift($log))
+                fwrite($fd, $msg."\n");
+
+        fclose($fd);
+
+    });
+
+    $cond = is_callable($cond) ? call_user_func($cond) : !!($cond);
+
+    return function () use ($cond, &$logs) {
+        return $cond ? $logs[] = func_get_args() : false;
+    };
 }
